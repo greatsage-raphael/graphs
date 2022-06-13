70b4122c6acefbcc58cdb3574e4714777bf66ef7
Collins Lubwama <bizzicole87@gmail.com>
2022-06-13 17:17:13 +0300

    graph practice

---
+++ index.js
@@ -0,0 +1,30 @@
+//A graph is a non-linear data structure consisting of nodes and edges.
+
+//Directed graph has direction while undirected graph has no direction.
+
+///adjancency list
+// {
+//     "A":["B", "D"],
+//     "B":["A", "C"],
+//     "C":["B", "D"],
+//     "D":["C", "A"],
+
+// }
+
+class Graph{
+    constructor() {
+        this.adjacencyList = {}
+    }
+    addVertex(name) {
+        //create a key with an empty array as the value   
+        this.adjacencyList[name] = [];
+    }
+}
+
+const graph = new Graph();
+
+graph.addVertex("M")
+graph.addVertex("Y")
+console.log(graph)
+
+
